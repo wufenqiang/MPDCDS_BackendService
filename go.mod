d6604bf511e325f09b71941661e5c6dad10da82c
wufenqiang <wing_cugb@163.com>
2020-06-01 18:09:36 +0800

    init

---
+++ go.mod
@@ -0,0 +1,18 @@
+module goserver-api
+
+ go 1.14
+
+require (
+	github.com/CloudyKit/fastprinter v0.0.0-20200109182630-33d98a066a53 // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/flosch/pongo2 v0.0.0-20200529170236-5abacdfa4915 // indirect
+	github.com/go-sql-driver/mysql v1.5.0
+	github.com/gorilla/schema v1.1.0 // indirect
+	github.com/iris-contrib/formBinder v5.0.0+incompatible // indirect
+	github.com/iris-contrib/middleware/jwt v0.0.0-20191219204441-78279b78a367
+	github.com/jinzhu/gorm v1.9.12
+	github.com/json-iterator/go v1.1.9
+	github.com/kataras/iris v11.1.1+incompatible
+	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
+	github.com/spf13/cast v1.3.1
+)
---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"flag"
+	"github.com/kataras/iris"
+	"goserver-api/conf"
+	"goserver-api/route"
+)
+
+func main() {
+	flag.Parse()
+	app := newApp()
+	route.InitRouter(app)
+	err := app.Run(iris.Addr(":"+conf.Sysconfig.Port), iris.WithoutServerError(iris.ErrServerClosed))
+	if err != nil {
+		panic(err)
+	}
+}
+
+//noinspection GoTypesCompatibility
+func newApp() *iris.Application {
+	app := iris.New()
+	//app.Configure(iris.WithOptimizations)
+	//crs := cors.New(cors.Options{
+	//	AllowedOrigins:   []string{"*"}, // allows everything, use that to change the hosts.
+	//	AllowCredentials: true,
+	//	AllowedHeaders:   []string{"*"},
+	//})
+	//app.Use(crs)
+	app.AllowMethods(iris.MethodOptions)
+	return app
+}
